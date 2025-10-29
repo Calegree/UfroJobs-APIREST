c72c71524ae5a2b03698b527118c510975f09175
Calegree <c.iturra07@ufromail.cl>
2025-10-28 21:38:46 -0300

    feat:swagger added

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('UfroJobs API')
+    .setDescription('Documentación de la API de UfroJobs')
+    .setVersion('1.0')
+    .addBearerAuth() // Para endpoints protegidos con JWT
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Conectar el microservicio para escuchar eventos de RabbitMQ
   app.connectMicroservice({
